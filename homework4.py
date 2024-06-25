2de0063d351ffb4fc2ff79fbf9886abddc5a4924
Алина Демчук <alina2805@bk.ru>
2024-06-25 14:47:35 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,5 +1,8 @@
-my_string = input("Здравствуйте, Алина")
+#my_string = input("Здравствуйте, Алина ")
 #print(my_string.upper())
 #print(my_string.lower())
-print(my_string[1])
+#print(my_string[0])
 #print(my_string[-1])
+name = "Вас приветствует Pycharm"
+print(name.replace("Вас приветствует Pycharm", "ВасприветствуетPycharm"))
+
